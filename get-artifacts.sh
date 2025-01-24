ea4e1992ad27c9b93cbb3b05a4e5f2fd9e8e7b14
user <user@debian>
2025-01-24 08:17:30 -0500

    WIP

--- get-artifacts.sh
+++ get-artifacts.sh
@@ -1,5 +1,14 @@
+TOKEN=$(<../token.gh)
+
+(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/Valouww/graphes/actions/artifacts) > gh-artifacts.json
+
+
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "Authorization: Bearer $TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/Valouww/graphes/actions/artifacts
+  https://api.github.com/repos/Valouww/graphes/actions/artifacts/2480744120/zip --output artifact.zip
